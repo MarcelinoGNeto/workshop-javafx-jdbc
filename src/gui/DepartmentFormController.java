30c521606d4deb9472f095d4c4720c076d1b5cd8
Marcelino Neto <marcelino.gneto@gmail.com>
2021-06-15 19:04:39 -0300

    DepartmentFormController

---
+++ src/gui/DepartmentFormController.java
@@ -0,0 +1,52 @@
+package gui;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+import gui.util.Constraints;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+
+public class DepartmentFormController implements Initializable {
+	
+	@FXML
+	private TextField txtId;
+	
+	@FXML
+	private TextField txtName;
+	
+	@FXML
+	private Label labelErrorName;
+	
+//Controles dos botões
+	@FXML
+	private Button btSave;
+	
+	@FXML
+	private Button btCancel;
+	
+//Métodos para tratar os eventos dos botões
+	@FXML
+	public void onBtSaveAction() {
+		System.out.println("OnBtSaveAction");
+	}
+
+	@FXML
+	public void onBtCancelAction() {
+		System.out.println("onBtCancelAction");
+	}
+	
+	@Override
+	public void initialize(URL url, ResourceBundle rb) {
+		initializeNodes();
+	}
+	
+	private void initializeNodes() {
+		Constraints.setTextFieldInteger(txtId); //aceitar apenas números inteiros
+		Constraints.setTextFieldMaxLength(txtName, 30);//determina a quantidade de caracteres
+	}
+
+}
